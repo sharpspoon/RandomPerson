5e856f57e5287a8b85991dce7fe94503e706bddf
Robin Ward <robin@steelcitysites.net>
2023-08-29 14:48:38 -0500

    update

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from datetime import date
+
 import pandas as pd
 import random
 from faker import Faker
@@ -9,12 +11,14 @@ def random_person_data(num_records, domain_name, file_type):
     first_names = [faker.first_name() for _ in range(num_records)]
     last_names = [faker.last_name() for _ in range(num_records)]
     middle_initials = [chr(random.randint(65, 90)) for _ in range(num_records)]
-    dob = [faker.date_between_dates(date_start="1950-01-01", date_end="2004-01-01") for _ in
+    dob = [faker.date_between_dates(date_start=date(1950, 1, 1), date_end=date(2004, 1, 1)) for _ in
            range(num_records)]
     phone_numbers = [faker.phone_number() for _ in range(num_records)]
 
     emails = [f"{first[:3].lower()}.{last.lower()}@{domain_name}" for first, last in
               zip(first_names, last_names)]
+
+    ssns = [faker.ssn() for _ in range(num_records)]
 
     df = pd.DataFrame({
         'First Name': first_names,
@@ -22,7 +26,8 @@ def random_person_data(num_records, domain_name, file_type):
         'Middle Initial': middle_initials,
         'DOB': dob,
         'Phone Number': phone_numbers,
-        'Email': emails
+        'Email': emails,
+        'SSN': ssns
     })
 
     if file_type == "xlsx":
@@ -35,7 +40,10 @@ def random_person_data(num_records, domain_name, file_type):
 
 # Usage
 num_records = 100  # Number of records you want to generate
-domain_name = "example.com"  # Domain name for email
-file_type = "xlsx"  # or 'csv'
+domain_name = "abdc.com"  # Domain name for email
+file_type = "csv"  # or 'xlsx'
 
-random_person_data(num_records, domain_name, file_type)
+if __name__ == '__main__':
+    random_person_data(num_records, domain_name, file_type)
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
